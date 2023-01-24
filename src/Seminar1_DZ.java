c7a17aeacc5fb9e099f7527ebe44182352fe9d5d
Evgeniy <zheinirk38@gmail.com>
2023-01-25 00:50:35 +0800

    added method triangleNum
    Метод вычисляет треугольное число

--- src/Seminar1_DZ.java
+++ src/Seminar1_DZ.java
@@ -2,15 +2,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+
+// Написать программу вычисления n-ого треугольного числа
+// Ввод числа с консоли
+// Вычисляю треугольное число
+// Вывожу найденное число в консоль
+
 public class Seminar1_DZ {
     public static void main(String[] args) throws IOException {
-        System.out.println(enterInt());
+        int num = enterInt();
+        System.out.println(num);
+        System.out.println(triangleNum(num));
     }
-    static int enterInt() throws IOException {
+
+    // Ввод числа с консоли
+    static int enterInt() throws IOException
+    {
         System.out.println("Введите целое число: ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String num = reader.readLine();
-        int enter = Integer.parseInt(num);
-        return enter;
+        return Integer.parseInt(num);
+    }
+
+    static int triangleNum(int num)
+    {
+        return num * (num + 1) / 2;
     }
 }
