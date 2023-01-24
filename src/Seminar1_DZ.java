be22b1595064dc0590999f86906cbcbc4c250cd6
Evgeniy <zheinirk38@gmail.com>
2023-01-25 00:40:32 +0800

    Added method enterInt
    Ввожу число для дальнейшего поиска треугольного числа

--- src/Seminar1_DZ.java
+++ src/Seminar1_DZ.java
@@ -1,2 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Seminar1_DZ {
+    public static void main(String[] args) throws IOException {
+        System.out.println(enterInt());
+    }
+    static int enterInt() throws IOException {
+        System.out.println("Введите целое число: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String num = reader.readLine();
+        int enter = Integer.parseInt(num);
+        return enter;
+    }
 }
